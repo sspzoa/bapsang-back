29133c2f019b1f3f12c825cb1299df1beb5a66f4
Seungpyo Suh <me@sspzoa.io>
2024-08-12 21:23:40 +0900

    plzz

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
-FROM python:3.12.5
+FROM python:3.12-apline
 
 WORKDIR /code
-
 COPY ./requirements.txt /code/requirements.txt
 
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 COPY ./app /code/app
 
-CMD ["uvicorn", "app.main:app"]
+CMD ["fastapi", "run", "app/main.py", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -1,28 +1,15 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 from typing import Union
 
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
