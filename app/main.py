df40640a2188d83e085a2a320f8df89d293c7e55
Seungpyo Suh <me@sspzoa.io>
2024-08-20 21:29:35 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,11 @@
 import os
-import logging
 from fastapi import FastAPI, HTTPException, UploadFile, File, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
 from openai import OpenAI
+import asyncio
 import json
 import uuid
-import aiohttp
-import requests
-
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY")
@@ -24,12 +18,6 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 app.mount("/uploads", StaticFiles(directory=UPLOAD_DIR), name="uploads")
 
-def is_url_accessible(url):
-    try:
-        response = requests.head(url)
-        return response.status_code == 200
-    except requests.RequestException:
-        return False
 
 @app.post("/chat")
 async def chat_with_gpt(request: Request, file: UploadFile = File(...)):
@@ -38,19 +26,13 @@ async def chat_with_gpt(request: Request, file: UploadFile = File(...)):
         unique_filename = f"{uuid.uuid4()}{file_extension}"
         file_path = os.path.join(UPLOAD_DIR, unique_filename)
 
-        contents = await file.read()
         with open(file_path, "wb") as buffer:
-            buffer.write(contents)
+            buffer.write(await file.read())
 
         base_url = str(request.base_url)
         if base_url.startswith("http://"):
             base_url = "https://" + base_url[7:]
         file_url = f"{base_url}uploads/{unique_filename}"
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(file_url) as response:
-                if response.status != 200:
-                    raise HTTPException(status_code=500, detail="File upload failed")
 
         text = """
         밥상의 중앙을 기준으로 각 음식들이 몇 시 방향에 있는지 구해줘.
@@ -65,7 +47,7 @@ async def chat_with_gpt(request: Request, file: UploadFile = File(...)):
         }
         """
 
-        print(f"file_url: {file_url}")
+        print(f"Image URL before sending to OpenAI: {file_url}")
 
         response = client.chat.completions.create(
             model="gpt-4o",
@@ -77,7 +59,7 @@ async def chat_with_gpt(request: Request, file: UploadFile = File(...)):
                         {
                             "type": "image_url",
                             "image_url": {
-                                "url": file_url,
+                                "url": f'"{base_url}"',
                             },
                         },
                     ],
